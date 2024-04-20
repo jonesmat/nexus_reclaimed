53ecacbc19fd87474b5d494870fb19ec07cbda6a
Matt Jones <matt.walker.jones@gmail.com>
2024-04-20 13:41:38 -0500

    Only move player if the Primary mouse button is clicked.

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,4 +1,5 @@
-import ppb  # type: ignore
+import ppb
+import ppb.buttons
 import math
 
 
@@ -17,21 +18,11 @@ class Player(ppb.Sprite):
 
         update.scene.main_camera.center = self.position
 
-    def on_button_pressed(self, event: ppb.events.ButtonPressed, signal):
-        print(f"Player: on_button_pressed: {type(event.button)} - {event.position}")
-
-        self.target_position = event.position
-
-    def move_to_target_position2(self, time_delta: float):
-        speed_tick = self.speed * time_delta
-        distance_to_target = math.dist(self.position, self.target_position)
-        move_ratio = min(speed_tick / distance_to_target, 1.0)
-
-        full_position_delta = self.target_position - self.position
-        position_delta = full_position_delta * move_ratio
-        self.position += position_delta
-
     def move_to_target_position(self, time_delta: float):
         speed_tick = self.speed * time_delta
         position_delta = self.target_position - self.position
         self.position += position_delta.truncate(speed_tick)
+
+    def on_button_pressed(self, event: ppb.events.ButtonPressed, signal):
+        if event.button is ppb.buttons.Primary:
+            self.target_position = event.position
