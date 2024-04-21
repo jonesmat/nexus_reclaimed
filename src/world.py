0023f3b472e90dffa102b58524269d6c86113d64
Matt Jones <matt.walker.jones@gmail.com>
2024-04-21 09:58:52 -0500

    Move loading entities to a WorldTile and support rotation.

--- src/world.py
+++ src/world.py
@@ -1,66 +1,35 @@
 from typing import List, Optional, Type
-import ppb
-from PIL import Image
-
+from ppb import Sprite, Vector, events, Scene
 from entities.boundary import Boundary
 from entities.grass import Grass
+from world_tile import WorldTile
 
 
-class WorldScene(ppb.Scene):
+class WorldScene(Scene):
 
-    def __init__(self, player_sprite: Type[ppb.Sprite], starting_map_tile_path: str, **kwargs):
+    def __init__(self, player_sprite: Type[Sprite], starting_map_tile_path: str, **kwargs):
         super().__init__(**kwargs)
 
         self.player_sprite = player_sprite
-        self.player: Optional[ppb.Sprite] = None
 
         self.offscreen_chunking_distance = 0
-        self.last_chuck_update_player_position = ppb.Vector(-1000, -1000)
-        self.offscreen_sprites: List[ppb.Sprite] = []
+        self.last_chuck_update_player_position = Vector(-1000, -1000)
+        self.offscreen_sprites: List[Sprite] = []
 
-        self._create_world_from_image(starting_map_tile_path)
+        self.current_tile = WorldTile(starting_map_tile_path, tile_orientation="WEST")
+        self.player = self.current_tile.load_player_sprite(self.player_sprite)
+        self.add(self.player)
 
-    WORLD_GEN_COLORS = {
-        "PLAYER": (0, 0, 255),  # BLUE
-        "GRASS": (0, 255, 0),  # GREEN
-        "BOUNDARY": (0, 0, 0),  # BLACK
-    }
+        self.offscreen_sprites.extend(self.current_tile.load_tile_sprites())
 
-    def _create_world_from_image(self, image_path: str):
-        with Image.open(image_path) as img:
-            pixels = img.load()
-            if not pixels:
-                raise RuntimeError()
-
-            width, height = img.size
-
-            for y in range(height):
-                for x in range(width):
-                    color = pixels[x, y]
-
-                    if color[3] == 0:  # TRANSPARENT
-                        pass
-
-                    elif color[:3] == WorldScene.WORLD_GEN_COLORS["PLAYER"] and not self.player:
-                        self.player = self.player_sprite(position=(x, y))
-                        self.add(self.player)
-
-                    elif color[:3] == WorldScene.WORLD_GEN_COLORS["GRASS"]:
-                        entity = Grass(position=(x, y))
-                        self.offscreen_sprites.append(entity)
-
-                    elif color[:3] == WorldScene.WORLD_GEN_COLORS["BOUNDARY"]:
-                        entity = Boundary(position=(x, y))
-                        self.offscreen_sprites.append(entity)
-
-    def on_update(self, event: ppb.events.Update, signal):
+    def on_update(self, event: events.Update, signal):
         self.offscreen_chunking_distance = (float(max(self.main_camera.width, self.main_camera.height)) * 1.5) / 2
         self._process_chunking()
 
         print(f"Items being rendered: {len(self.children)}")
 
     def _process_chunking(self):
-        if self.player and (self.player.position - self.last_chuck_update_player_position).length > (
+        if (self.player.position - self.last_chuck_update_player_position).length > (
             self.offscreen_chunking_distance / 10
         ):
             self.last_chuck_update_player_position = self.player.position
@@ -69,9 +38,9 @@ class WorldScene(ppb.Scene):
 
     def _chunk_load_offscreen_sprites(self):
         # Check for "offscreen" sprites that need to be added to the scene.
-        sprites_remaining_offscreen: List[ppb.Sprite] = []
+        sprites_remaining_offscreen: List[Sprite] = []
         for sprite in self.offscreen_sprites:
-            if self.player and (sprite.position - self.player.position).length <= self.offscreen_chunking_distance:
+            if (sprite.position - self.player.position).length <= self.offscreen_chunking_distance:
                 self.add(sprite)
             else:
                 sprites_remaining_offscreen.append(sprite)
--- src/world_tile.py
+++ src/world_tile.py
@@ -1,2 +1,74 @@
+from typing import List, Type
+from ppb import Sprite
+from PIL import Image
+from entities.boundary import Boundary
+from entities.grass import Grass
+
+WORLD_GEN_COLORS = {
+    "PLAYER": (0, 0, 255),  # BLUE
+    "GRASS": (0, 255, 0),  # GREEN
+    "BOUNDARY": (0, 0, 0),  # BLACK
+}
+
+TILE_ORIENTATION_ROTATION_ANGLE = {"NORTH": 0, "EAST": 90, "SOUTH": 180, "WEST": -90}
+
+
 class WorldTile:
     """Handle"""
+
+    def __init__(self, tile_image_path: str, **kwargs):
+
+        self._tile_image_path = tile_image_path
+        self.tile_orientation = kwargs.get("tile_orientation", "NORTH")
+
+    def load_player_sprite(self, player_sprite: Type[Sprite]) -> Sprite:
+        with Image.open(self._tile_image_path) as img:
+
+            rotation_angle = TILE_ORIENTATION_ROTATION_ANGLE.get(self.tile_orientation, 0)
+            rotated_img = img.rotate(rotation_angle, expand=True)
+
+            pixels = rotated_img.load()
+            if not pixels:
+                raise RuntimeError()
+
+            width, height = rotated_img.size
+
+            for y in range(height):
+                for x in range(width):
+                    color = pixels[x, y]
+
+                    if color[:3] == WORLD_GEN_COLORS["PLAYER"]:
+                        return player_sprite(position=(x, y))
+
+            # Unable to find the player pixel in the image
+            raise RuntimeError("Unable to find the player position")
+
+    def load_tile_sprites(self) -> List[Sprite]:
+        sprites: List[Sprite] = []
+
+        with Image.open(self._tile_image_path) as img:
+            rotation_angle = TILE_ORIENTATION_ROTATION_ANGLE.get(self.tile_orientation, 0)
+            rotated_img = img.rotate(rotation_angle, expand=True)
+
+            pixels = rotated_img.load()
+            if not pixels:
+                raise RuntimeError()
+
+            width, height = rotated_img.size
+
+            for y in range(height):
+                for x in range(width):
+                    color = pixels[x, y]
+
+                    if color[3] == 0:  # TRANSPARENT
+                        pass
+
+                    elif color[:3] == WORLD_GEN_COLORS["GRASS"]:
+                        entity = Grass(position=(x, y))
+                        sprites.append(entity)
+
+                    elif color[:3] == WORLD_GEN_COLORS["BOUNDARY"]:
+                        entity = Boundary(position=(x, y))
+                        sprites.append(entity)
+
+        return sprites
